5a33cd261e195a2c1200bcf6bd333fbe0ca85793
NotGreyhat <notgreyhat@gmail.com>
2022-03-20 22:25:43 -0400

    Added helper.

--- src/hello.c
+++ src/hello.c
@@ -1,4 +1,16 @@
 #include <efi.h>
+#include <stddef.h>
+
+
+int memcmp(const void* aptr, const void* bptr, size_t n) {
+	const unsigned char* a = aptr, *b = bptr;
+	for (size_t i = 0; i < n; i++) {
+		if (a[i] < b[i]) return -1;
+		else if (a[i] > b[i]) return 1;
+	}
+	return 0;
+}
+
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     (void)ImageHandle;
